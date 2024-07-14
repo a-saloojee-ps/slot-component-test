7c95ead2f08f47a8cadfc7128b18fcdc241fe8d5
a-saloojee-ps <asaloojee@pre-script.com>
2024-07-14 22:58:50 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeroElement from './components/hero-element/HeroElement.vue'
+</script>
+
+<template>
+    <HeroElement
+        bg-image="https://storage.googleapis.com/pre-script-web-public/lp/l1/l1-webinar-footer.png"
+    >
+        <template #text> Slot-induced header! </template>
+    </HeroElement>
+</template>
---
+++ src/components/hero-element/HeroElement.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed } from 'vue'
+
+import './hero-element.css'
+
+const props = defineProps({
+  bgImage: {
+    type: String,
+    required: true
+  }
+})
+
+const imageBgStyle = computed(() => {
+  console.log(props.bgImage)
+
+  return {
+    backgroundImage: `url(${props.bgImage})`
+  }
+})
+</script>
+
+<template>
+  <div class="hero-element" :style="imageBgStyle">
+    <div class="hero-element__content">
+      <div class="hero-element__content__header">
+        <slot name="text"> </slot>
+      </div>
+      <div class="hero-element__content__text">
+        This is some supporting text to show you how cool I am. I'm so cool that I can blow your
+        socks off with some simple dummy text. How cool is that?
+      </div>
+      <div class="hero-element__content__cta">
+        <button>Click Me to Be Cool</button>
+      </div>
+    </div>
+  </div>
+</template>
